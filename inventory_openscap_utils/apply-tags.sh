90b8e4959e60da81a3fecac610d7e8147228fe07
straysheep-dev <bah@straysheep.dev>
2024-07-10 06:03:52 -0700

    Update inventory_openscap_utils

--- inventory_openscap_utils/apply-tags.sh
+++ inventory_openscap_utils/apply-tags.sh
@@ -2,34 +2,131 @@
 
 # GPL-3.0 License
 
-# This script will apply the tags in $1 to the playbook list. It's meant to make
-# combining policies and playbooks easier.
-# Change the Ansible command in the for loop to match your site's needs. By default it
-# executes everything locally, with -C and -D to show a diff without applying changes.
+# This script will apply all uncommented tags found in `pwd`/tags-*.txt files using the
+# playbooks in `pwd`/playbook-list.txt file.
+# It's meant to make combining policies and playbooks easier.
 
+RED="\033[01;31m"      # Issues/Errors
+GREEN="\033[01;32m"    # Success
+YELLOW="\033[01;33m"   # Warnings
+BLUE="\033[01;34m"     # Information
+BOLD="\033[01;01m"     # Highlight
+RESET="\033[00m"       # Normal
+
+# Hardcoded variables based on the download path created with download-content.sh
 DOWNLOAD_PATH=~/src
 AUTHOR_REPO='ComplianceAsCode/content'
 content_path=$(find "$DOWNLOAD_PATH"/"$AUTHOR_REPO"/scap-security-guide-* -maxdepth 0 -type d -print | sort -n | head -n 1)
-TAG_FILE="$1"
-PLAYBOOK_LIST_FILE='playbook-list.txt'
-
-if [[ "$1" == '' ]]; then
-    echo "[*]Usage: $0 [<tag-file>.txt]"
-    exit 1
-fi
-
-if [[ ! -e "$1" ]]; then
-    echo "[*]Tag file not found. Quitting."
-    exit 1
-fi
+TAGS_LIST=$(find ../ -maxdepth 2 -name "tags-*.txt" | awk -F'/' '{print $3}')
+PLAYBOOK_LIST=$(cat playbook-list.txt)
+# Initialize argument variables
+CHECK_DIFF=''
+INVENTORY_PATH=''
+VAULT_PATH=''
+BECOME_ARGS=''
+LOCAL_CONNECT=''
 
 # shellcheck source=/dev/null
 source /etc/os-release
 
-for playbook in $(cat "$PLAYBOOK_LIST_FILE");
-do
-    echo "[*]Applying tags:$1  playbook:$(echo $playbook | awk -F'/' '{print $NF}')..."
-    # Loops through the list of playbooks used to compile the tag lists, applies the set of tags from $1 using those playbooks
-    ansible-playbook -i "localhost," -c local -b --ask-become-pass "$content_path"/ansible/"$playbook" --tags $(grep -Pv "^#" < "$1" | tr '\n' ',') -C -D
-    #ansible-playbook -i inventory.ini -b -e "@auth.yml" --ask-vault-pass "$content_path"/ansible/"$playbook" --tags $(grep -Pv "^#" < "$1" | tr '\n' ',') -C -D
+HelpMenu() {
+    echo ""
+    echo "[*]Usage: $0 -i <inventory>"
+    echo ""
+    echo "    [-d|--diff] Run playbook in -C check and -D diff mode"
+    echo "    [-l|--local] Connect locally, -i must be 'localhost,'"
+    echo "    [-v|--vault /path/to/vault.yml] Path to a vault file"
+    echo "    [-b|--become] Elevate entire playbook to root"
+	echo ""
+	echo "Example: $0 -i \"localhost,\" --vault \"/home/fedora/src/vault.yml\" --local --diff"
+	echo "Example: $0 -i ~/ansible/inventory.ini --vault \"~/ansible/vault.yml\" "
+    echo ""
+    exit 1
+}
+
+ExecutePlaybooks() {
+    for playbook in $PLAYBOOK_LIST;
+    do
+        for tag_file in $TAGS_LIST;
+        do
+			active_tags=$(grep -Pv '^(#|$)' < "$tag_file" | tr '\n' ',')
+			if [[ $(grep -Pv '^(#|$)' $tag_file | wc -l | awk '{print $1}') != '0' ]]; then
+				echo -e "${BLUE}[*]Applying tags:$tag_file  playbook:$playbook...${RESET}"
+				# Loops through the list of playbooks used to compile the tag lists, applies the set of tags from $1 using those playbooks
+
+				# These vairables cannot be double quoted
+				echo -e "${BLUE}"
+				echo PLAYBOOK COMMAND: ansible-playbook ${INVENTORY_ARGS} ${LOCAL_CONNECT} "${VAULT_ARGS}" ${BECOME_ARGS} $content_path/ansible/$playbook --tags $active_tags ${CHECK_DIFF}
+				echo -e "${RESET}"
+				# This must be echo'd and piped to | bash
+				echo ansible-playbook ${INVENTORY_ARGS} ${LOCAL_CONNECT} "${VAULT_ARGS}" ${BECOME_ARGS} $content_path/ansible/$playbook --tags $active_tags ${CHECK_DIFF} | bash || echo "[ERROR] Quitting."; exit 1
+			else
+				echo -e "${BLUE}[*]$tag_file has no active tags. Skipping...${RESET}"
+			fi
+        done
+    done
+}
+
+# This is the easiest way to do this in bash, but it won't work in other shells
+# See getopt-parse under /usr/share/doc/util-linux/examples
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-b|--become)
+			BECOME_ARGS="-b"
+			shift # past argument
+			#shift # Commented, since argument has no value to shift past
+			;;
+		-d|--diff)
+			CHECK_DIFF="-C -D"
+			shift # past argument
+			#shift # Commented, since argument has no value to shift past
+			;;
+		-i|--inventory)
+			INVENTORY_ARGS="-i $2"
+			shift # past argument
+			shift # past value
+			;;
+		-l|--local)
+			LOCAL_CONNECT="-c local"
+			shift # past argument
+			#shift # Commented, since argument has no value to shift past
+			;;
+		-v|--vault)
+			VAULT_ARGS="-e \"@$2\" --vault-pass-file <(cat <<<\$ANSIBLE_VAULT_PASSWORD)"
+			shift # past argument
+			shift # past value
+			;;
+		-h|--help)
+			HelpMenu
+			exit 0
+			shift # past argument
+			#shift # Commented, since argument has no value to shift past
+			;;
+		-*|--*)
+			echo "Unknown option $1"
+			exit 1
+			;;
+		*)
+			POSITIONAL_ARGS+=("$1") # save positional arg
+			shift # past argument
+			;;
+	esac
 done
+
+echo ""
+echo -e "${BLUE}INVENTORY_ARGS: $INVENTORY_ARGS${RESET}"
+echo -e "${BLUE}VAULT_ARGS: $VAULT_ARGS${RESET}"
+echo -e "${BLUE}BECOME_ARGS: $BECOME_ARGS${RESET}"
+echo -e "${BLUE}LOCAL_CONNECT: $LOCAL_CONNECT${RESET}"
+echo -e "${BLUE}CHECK_DIFF: $CHECK_DIFF${RESET}"
+echo ""
+
+# Get the vault password if using a vault
+if [[ "$ANSIBLE_VAULT_PASSWORD" == '' ]]; then
+	echo "Enter Vault Password (or [enter] if not using a vault)"; read -s vault_pass; export ANSIBLE_VAULT_PASSWORD=$vault_pass
+fi
+
+ExecutePlaybooks
--- inventory_openscap_utils/get-tags.sh
+++ inventory_openscap_utils/get-tags.sh
@@ -15,15 +15,24 @@ PLAYBOOK_LIST_FILE='playbook-list.txt'
 PLAYBOOK_REGEX="$1"
 if [[ "$1" =~ ^(-h|--help)$ ]]; then
 	# Show usage if $1 equals -h or --help
+	echo ""
 	echo "This script will locate the latest version of ComplianceAsCode/content's Ansible playbooks"
 	echo "and extract all available tags into a single list, sorting that list into separate groups."
 	echo ""
-	echo "As an optional argument, $0 reads \$1 and tries to match it against an existing playbook using"
+	echo "Use -u|--unzip <os-name> to unzip policy playbooks for other OS's (example: debian) if blank, will unzip all playbooks."
+	echo "Use -l|--list to list all avaialble policy playbooks."
+	echo "Use -r|--remove to delete all generated tags-*.txt and playbook-*.txt lists from the current directory."
+	echo "Use -h|--help to print this help."
+	echo ""
+	echo "Without any dash options, $0 reads \$1 and tries to match it against an existing playbook using"
 	echo "find's posix-extended regex parser."
 	echo ""
+	echo "  Example usage: $0 -l"
+	echo "  Example usage: $0 -u debian"
 	echo "	Exmaple usage: $0 pci"
-	echo "	Example usage: $0 'ubuntu2.*standard'"
+	echo "	Example usage: $0 'debian10.*'"
 	echo "	Exmaple usage: $0 '2204.*(stig|cis_level[0-9]_workstation)'"
+	echo ""
 	exit 0
 fi
 
@@ -32,6 +41,33 @@ source /etc/os-release
 
 content_path=$(find "$DOWNLOAD_PATH"/"$AUTHOR_REPO"/scap-security-guide-* -maxdepth 0 -type d -print | sort -n | head -n 1)
 echo "[*]Found $content_path"
+
+if [[ "$1" =~ ^(-l|--list|)$ ]]; then
+	playbook_list=$(find "$content_path"/ansible/ -regextype posix-extended -iregex ".*/.*\.yml" | sort)
+	echo ""
+	echo "[*]Available policies:"
+	for playbook in $playbook_list; do
+		echo "  - $(basename "$playbook")"
+	done
+	exit 0
+fi
+
+if [[ "$1" =~ ^(-u|--unzip)$ ]]; then
+	echo "[>]Extracting content for ${2}..."
+	cd "$DOWNLOAD_PATH"/"$AUTHOR_REPO" || exit
+	unzip scap-security-guide-*.zip "*ansible/$2*" "*ssg-$2*xml" || echo "[ERROR] Quitting."; exit 1
+	exit 0
+fi
+
+if [[ "$1" =~ ^(-r|--remove)$ ]]; then
+	current_tags=$(find ../ -maxdepth 2 -regextype posix-extended -iregex ".*(tags-|playbook-).*.txt" | sort)
+	echo "[>]Removing all generated tag and playbook list files..."
+	for tag_file in $current_tags; do
+		echo "  [-] $(basename "$tag_file")"
+		rm -f "$tag_file" 2>/dev/null
+	done
+	exit 0
+fi
 
 if [[ "$1" != "" ]]; then
 	# If $1 has an argument, try to find a matching playbook
@@ -42,9 +78,6 @@ if [[ "$1" != "" ]]; then
 		echo "[WARNING]No playbook matching \"$1\""
 		exit 1
 	fi
-else
-	# If $1 has no argument, check all available playbooks
-	playbook_list="$content_path/ansible/*${ID}*.yml"
 fi
 
 rm "$PLAYBOOK_LIST_FILE" 2>/dev/null # Remove any previous list to start a new one
@@ -57,70 +90,71 @@ done
 
 sort < tags.tmp | uniq | tee "$TAG_FILE" >/dev/null
 rm tags.tmp
-echo "[>]Wrote tags to $TAG_FILE."
-echo "[>]Wrote playbooks used to $PLAYBOOK_LIST_FILE"
+echo "  [>] Wrote tags to $TAG_FILE."
+echo "  [>] Wrote playbooks used to $PLAYBOOK_LIST_FILE"
 
 # Parse account related tags
 tag_group_file='tags-accounts'
 grep 'password' tags-all.txt | tee $tag_group_file.tmp > /dev/null
 grep 'accounts' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
 grep 'pam' tags-all.txt | grep -Pv "(sshd|audit_)"| tee -a $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse filesystem mount tags
 tag_group_file='tags-mount-options'
 grep -P '^mount_option_' tags-all.txt | tee $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse service-enabled tags
 tag_group_file='tags-services-enabled'
 grep -P '^service_.*_enabled$' tags-all.txt | tee $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse service-disabled tags
 tag_group_file='tags-services-disabled'
 grep -P '^service_.*_disabled$' tags-all.txt | tee $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse package-installed tags
 tag_group_file='tags-packages-installed'
 grep -P '^package_.*_installed$' tags-all.txt | tee $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse package-removed tags
 tag_group_file='tags-packages-removed'
 grep -P '^package_.*_removed$' tags-all.txt | tee $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse filesystem tags
 tag_group_file='tags-filesystem'
 grep -P '^dir_.*$' tags-all.txt | tee $tag_group_file.tmp > /dev/null
 grep -P '^file_.*$' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse kernel related tags
 tag_group_file='tags-kernel'
 grep -P '^kernel_module_.*' tags-all.txt | tee $tag_group_file.tmp > /dev/null
 grep -P '^sysctl_.*' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
+grep -P '^coredump.*' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
 grep -P 'disable_ctrlaltdel_.*$' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
 grep 'disable_users_coredumps' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse sshd related tags
 tag_group_file='tags-sshd'
@@ -128,30 +162,30 @@ grep 'sshd' tags-all.txt | tee $tag_group_file.tmp > /dev/null
 grep 'banner' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
 grep 'package_openssh-server_installed' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
 grep 'disable_host_auth' tags-all.txt | tee -a $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse aide related tags
 tag_group_file='tags-aide'
 grep -P '^aide_.*$' tags-all.txt | tee $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse auditd related tags
 tag_group_file='tags-auditd'
 grep -P '^audit_.*$' tags-all.txt | tee $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Parse sudo related tags
 tag_group_file='tags-sudo'
 grep -P '^sudo_.*$' tags-all.txt | tee $tag_group_file.tmp > /dev/null
-sort < $tag_group_file.tmp | tee $tag_group_file.txt > /dev/null
+sort < $tag_group_file.tmp | uniq | tee $tag_group_file.txt > /dev/null
 rm $tag_group_file.tmp
-echo "[>]Created $tag_group_file.txt..."
+echo "  [>] Created $tag_group_file.txt..."
 
 # Get a line count for each file
 print_line_count() {
@@ -160,3 +194,6 @@ print_line_count() {
 	done
 }
 print_line_count | column -t
+
+# Automatically comment tags-all.txt
+sed -i 's/^/#/g' tags-all.txt
